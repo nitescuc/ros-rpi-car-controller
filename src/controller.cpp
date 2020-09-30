d5778c31d54f74c78653887b2013969d5a75d8cb
Cristian Nitescu <nitescuc@hotmail.com>
2020-09-30 18:27:59 +0100

    Simplified configuration

--- src/controller.cpp
+++ src/controller.cpp
@@ -4,19 +4,19 @@
 #include "sensor_msgs/Joy.h"
 #include "car_actuators/Stop.h"
 
-ros::Publisher steering_publisher, pid_enable_publisher, manual_throttle_publisher, pilot_throttle_publisher, *crt_throttle_publisher;
+ros::Publisher steering_publisher, pid_enable_publisher, throttle_publisher;
 int steering_axe = 0, throttle_axe = 1, mode_button = 0;
 int remote_mode = 0, pilot_mode = 0;
+bool block_reverse = false;
 double attenuation = 0.8;
 double steering = 0.0, throttle = 0.0;
 ros::ServiceClient stop_client;
 
-void publish_throttle(ros::Publisher *publisher, double throttle);
+void publish_throttle(double throttle);
 void publish_steering(double steering);
 
 bool isAutoSteering() { return pilot_mode == 1; }
 bool isAutoThrottle() { return pilot_mode == 1 &&  remote_mode == 1; }
-bool activatePID() { return remote_mode == 1; }
 
 void enable_pid(bool enabled)
 {
@@ -33,15 +33,10 @@ void setConfiguration(int in_pilot_mode, int in_remote_mode)
     bool change_remote_mode = (remote_mode != in_remote_mode);
     pilot_mode = in_pilot_mode;
     remote_mode = in_remote_mode;
-    if (isAutoThrottle() || activatePID())
-    {
-        crt_throttle_publisher = &pilot_throttle_publisher;
-    } else {
-        crt_throttle_publisher = &manual_throttle_publisher;
-    }
+
     if (change_pilot_mode || change_remote_mode) 
     {
-        enable_pid(activatePID());
+        publish_throttle(0.0);
         if (!isAutoThrottle() && isAutoSteering())
         {
             stop_req.request.stop = true;
@@ -57,7 +52,7 @@ void remote_callback(const sensor_msgs::Joy joy)
     if (! isAutoThrottle())
     {
         throttle = joy.axes[throttle_axe];
-        publish_throttle(crt_throttle_publisher, throttle);
+        publish_throttle(throttle);
     }
     if (! isAutoSteering()) 
     {
@@ -77,16 +72,23 @@ void pilot_callback(const sensor_msgs::Joy joy)
     if (isAutoThrottle())
     {
         throttle = joy.axes[throttle_axe];
-        publish_throttle(&pilot_throttle_publisher, throttle);
+        publish_throttle(throttle);
     }
 }
 
-void publish_throttle(ros::Publisher *publisher, double throttle)
+void publish_throttle(double throttle)
 {
     std_msgs::Float64 drive;
-    drive.data = throttle;
-    
-    publisher->publish(drive);
+    if (block_reverse && (throttle < 0))
+    {
+        drive.data = 0;
+    } 
+    else
+    {
+        drive.data = throttle;
+    }
+
+    throttle_publisher.publish(drive);
 }
 
 void publish_steering(double steering)
@@ -102,23 +104,22 @@ int main(int argc, char** argv)
     // Initialize the process_image node and create a handle to it
     ros::init(argc, argv, "controller");
     ros::NodeHandle n;
-    ros::NodeHandle pH("~controller");
+    ros::NodeHandle pH("~");
 
     pH.param("axes/steering", steering_axe, 0);
     pH.param("axes/throttle", throttle_axe, 1);
     pH.param("buttons/mode", mode_button, 0);
-    pH.param("attenuation", attenuation, 0.8);
+    pH.param("block_reverse", block_reverse, false);
 
-    manual_throttle_publisher = n.advertise<std_msgs::Float64>("/actuator/throttle", 1);
-    pilot_throttle_publisher = n.advertise<std_msgs::Float64>("/actuator/auto_throttle", 1);
-    steering_publisher = n.advertise<std_msgs::Float64>("/actuator/steering", 1);
+    throttle_publisher = n.advertise<std_msgs::Float64>("/controller/throttle", 1);
+    steering_publisher = n.advertise<std_msgs::Float64>("/controller/steering", 1);
     pid_enable_publisher = n.advertise<std_msgs::Bool>("/pid_enable", 1);
 
     // Subscribe to /actuator/drive
     ros::Subscriber remote = n.subscribe("/remote", 1, remote_callback);   
     ros::Subscriber pilot = n.subscribe("/pilot", 1, pilot_callback);
 
-    enable_pid(false);
+//    enable_pid(true);
 
     // Handle ROS communication events
     ros::spin();
